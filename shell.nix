8d6f2ed91df33a58abc54989806a458d6cbb0b24
Ezri <me@ezrizhu.com>
2026-08-01 23:11:21 -0400

    init

---
+++ shell.nix
@@ -0,0 +1,19 @@
+{ pkgs ? (import <nixpkgs> { # we can vendor this and run our own cache
+    config.allowUnfree = true; # for terraform
+}), ... }:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    vim
+    git
+    kubectl
+    talosctl
+  ];
+  shellHook = ''
+    export GIT_CONFIG_COUNT=2
+    export GIT_CONFIG_KEY_0="user.name"
+    export GIT_CONFIG_VALUE_0="Ezri"
+    export GIT_CONFIG_KEY_1="user.email"
+    export GIT_CONFIG_VALUE_1="me@ezrizhu.com"
+  '';
+}
